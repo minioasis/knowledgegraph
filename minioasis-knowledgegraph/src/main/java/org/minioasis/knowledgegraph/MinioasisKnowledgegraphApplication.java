4d02c73caa38215ad3e49badc1aa565b8e0c16cb
minioasis <minioasis@gmail.com>
2020-03-21 18:23:32 +0800

    1st time commit

---
+++ minioasis-knowledgegraph/src/main/java/org/minioasis/knowledgegraph/MinioasisKnowledgegraphApplication.java
@@ -0,0 +1,13 @@
+package org.minioasis.knowledgegraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioasisKnowledgegraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinioasisKnowledgegraphApplication.class, args);
+	}
+
+}
---
+++ minioasis-knowledgegraph/src/test/java/org/minioasis/knowledgegraph/MinioasisKnowledgegraphApplicationTests.java
@@ -0,0 +1,13 @@
+package org.minioasis.knowledgegraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioasisKnowledgegraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
