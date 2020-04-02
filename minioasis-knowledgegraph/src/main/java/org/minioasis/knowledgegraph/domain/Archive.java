cc2156c2c21d94b5802e7fb864c9431e7ceaf10e
minioasis <minioasis@gmail.com>
2020-04-02 21:03:08 +0800

    completed CRUD actions

--- minioasis-knowledgegraph/src/main/java/org/minioasis/knowledgegraph/domain/Archive.java
+++ minioasis-knowledgegraph/src/main/java/org/minioasis/knowledgegraph/domain/Archive.java
@@ -1,10 +1,13 @@
 package org.minioasis.knowledgegraph.domain;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
 
 @NodeEntity
 public class Archive {
@@ -18,6 +21,9 @@ public class Archive {
 	private String content;	
 	private String ref;
 	private LocalDateTime created;
+	
+	@Relationship(type = "REFER_TO", direction = Relationship.UNDIRECTED)
+	private List<Archive> archives = new ArrayList<Archive>();
 	
 	public Long getId() {
 		return id;
@@ -60,6 +66,12 @@ public class Archive {
 	}
 	public void setCreated(LocalDateTime created) {
 		this.created = created;
+	}
+	public List<Archive> getArchives() {
+		return archives;
+	}
+	public void setArchives(List<Archive> archives) {
+		this.archives = archives;
 	}
 
 }
